9a58c8fe7e5e33edb9c8f283bb280ce1f8d00632
ShivaBhattacharjee <shivabhattacharjee@icloud.com>
2026-04-10 10:32:20 +0530

    add: location api route

--- src/app/api/location/route.js
+++ src/app/api/location/route.js
@@ -2,11 +2,12 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   const city = request.headers.get("x-vercel-ip-city") || "";
-  const country = request.headers.get("x-vercel-ip-country-region") || "";
+  const region = request.headers.get("x-vercel-ip-country-region") || "";
   const countryName = request.headers.get("x-vercel-ip-country") || "";
 
   return NextResponse.json({
     city: decodeURIComponent(city),
     country: countryName,
+    region: region,
   });
 }
